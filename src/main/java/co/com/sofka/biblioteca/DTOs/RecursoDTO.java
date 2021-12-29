acebd3a8ecc17710aeaa0eb1202b31c48515cceb
David H. Angarita <davidangarita.sistemas@gmail.com>
2021-12-28 22:09:30 -0500

    DTO y Clase recurso creadas

---
+++ src/main/java/co/com/sofka/biblioteca/DTOs/RecursoDTO.java
@@ -0,0 +1,67 @@
+package co.com.sofka.biblioteca.DTOs;
+
+import co.com.sofka.biblioteca.Enums.AreaTematica;
+import co.com.sofka.biblioteca.Enums.Tipo;
+
+import java.util.Date;
+
+public class RecursoDTO {
+
+    private String id;
+    private String nombre;
+    private boolean isDisponible = true;
+    private Date fechaPrestamo;
+    private AreaTematica areaTematica;
+    private Tipo tipo;
+
+    public RecursoDTO() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isDisponible() {
+        return isDisponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        isDisponible = disponible;
+    }
+
+    public Date getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public void setFechaPrestamo(Date fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+
+    public AreaTematica getAreaTematica() {
+        return areaTematica;
+    }
+
+    public void setAreaTematica(AreaTematica areaTematica) {
+        this.areaTematica = areaTematica;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+}
---
+++ src/main/java/co/com/sofka/biblioteca/Models/Recurso.java
@@ -0,0 +1,77 @@
+package co.com.sofka.biblioteca.Models;
+
+import co.com.sofka.biblioteca.Enums.AreaTematica;
+import co.com.sofka.biblioteca.Enums.Tipo;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Document("Recurso")
+public class Recurso {
+
+    @Id
+    private String id;
+    private String nombre;
+    private boolean isDisponible = true;
+    private Date fechaPrestamo;
+    private AreaTematica areaTematica;
+    private Tipo tipo;
+
+    public Recurso() {
+    }
+
+    public Recurso(String nombre, AreaTematica areaTematica, Tipo tipo) {
+        this.nombre = nombre;
+        this.areaTematica = areaTematica;
+        this.tipo = tipo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isDisponible() {
+        return isDisponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        isDisponible = disponible;
+    }
+
+    public Date getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public void setFechaPrestamo(Date fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+
+    public AreaTematica getAreaTematica() {
+        return areaTematica;
+    }
+
+    public void setAreaTematica(AreaTematica areaTematica) {
+        this.areaTematica = areaTematica;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+}
