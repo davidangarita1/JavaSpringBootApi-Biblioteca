1146b6fdbb0c11d67e54ccb926d319db3087a1af
David H. Angarita <davidangarita.sistemas@gmail.com>
2021-12-28 22:09:57 -0500

    Enums creados

---
+++ src/main/java/co/com/sofka/biblioteca/Enums/AreaTematica.java
@@ -0,0 +1,13 @@
+package co.com.sofka.biblioteca.Enums;
+
+public enum AreaTematica {
+    CIENCIAS,
+    NATURALEZA,
+    HISTORIA,
+    MATEMATICAS,
+    LENGUAS,
+    TECNOLOGIA,
+    ARTES,
+    FILOSOFIA,
+    DEPORTES
+}
---
+++ src/main/java/co/com/sofka/biblioteca/Enums/Tipo.java
@@ -0,0 +1,10 @@
+package co.com.sofka.biblioteca.Enums;
+
+public enum Tipo {
+    LIBRO,
+    REVISTA,
+    FICHAS,
+    PELICULAS,
+    PRENSA,
+    DUCOMENTOS
+}
